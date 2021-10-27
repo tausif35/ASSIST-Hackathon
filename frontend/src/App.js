9ba52d72f9a86eabb6c25c245580c522b9ae41e1
Tausif Ahmed <tausif@cuebites.com>
2021-10-27 19:43:47 +0600

    initial push

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/Routes';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
