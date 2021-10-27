1dabf0bc2e5d5c645de1430ea9291b8c1b92ff24
Sajidul Kabir <sajidulkabiradib@gmail.com>
2021-10-27 22:01:23 +0600

    Better error handling

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/AppError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 app.use(express.json());
@@ -13,7 +16,15 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("hoooas");
+//REST Architecture
+app.use("/api/users", userRouter);
+
+//Error Handling for all undefined routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+//Global Error handler
+app.use(globalErrorHandler);
+
 module.exports = app;
